a55399d8916639e12d094b5439cc8b4b461019b8
James Reeves <jreeves@weavejester.com>
2016-12-16 03:28:02 +0000

    Add tests for init and halt!

--- project.clj
+++ project.clj
@@ -5,4 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [integrant "0.1.4"]
-                 [ring/ring-jetty-adapter "1.5.0"]])
+                 [ring/ring-jetty-adapter "1.5.0"]]
+  :profiles
+  {:dev {:dependencies [[clj-http "2.1.0"]]}})
--- test/duct/server/jetty_test.clj
+++ test/duct/server/jetty_test.clj
@@ -1,7 +1,21 @@
 (ns duct.server.jetty-test
-  (:require [clojure.test :refer :all]
-            [duct.server.jetty :refer :all]))
+  (:import java.net.ConnectException)
+  (:require [clj-http.client :as http]
+            [clojure.test :refer :all]
+            [duct.server.jetty :refer :all]
+            [integrant.core :as ig]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest init-and-halt-test
+  (let [response {:status 200 :headers {} :body "test"}
+        handler  (constantly response)
+        config   {:duct.server/jetty {:port 3400, :handler handler}}]
+    (testing "server starts"
+      (let [system (ig/init config)]
+        (try
+          (let [response (http/get "http://127.0.0.1:3400/")]
+            (is (= (:status response) 200))
+            (is (= (:body response) "test")))
+          (finally
+            (ig/halt! system)))))
+    (testing "server stops"
+      (is (thrown? ConnectException (http/get "http://127.0.0.1:3400/"))))))
