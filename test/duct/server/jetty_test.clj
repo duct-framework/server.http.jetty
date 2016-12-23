6f2e5fbee2b0ffa849b1d53fb23a37514d92e65c
James Reeves <jreeves@weavejester.com>
2016-12-23 02:56:18 +0000

    Add tests for suspend! and resume

--- test/duct/server/jetty_test.clj
+++ test/duct/server/jetty_test.clj
@@ -19,3 +19,47 @@
             (ig/halt! system)))))
     (testing "server stops"
       (is (thrown? ConnectException (http/get "http://127.0.0.1:3400/"))))))
+
+(deftest resume-and-suspend-test
+  (let [response1 {:status 200 :headers {} :body "foo"}
+        response2 {:status 200 :headers {} :body "bar"}
+        config1   {:duct.server/jetty {:port 3400, :handler (constantly response1)}}
+        config2   {:duct.server/jetty {:port 3400, :handler (constantly response2)}}]
+
+    (testing "suspend and resume"
+      (let [system1  (doto (ig/init config1) ig/suspend!)
+            response (future (http/get "http://127.0.0.1:3400/"))
+            system2  (ig/resume config2 system1)]
+        (try
+          (is (identical? (-> system1 :duct.server/jetty :handler)
+                          (-> system2 :duct.server/jetty :handler)))
+          (is (identical? (-> system1 :duct.server/jetty :server)
+                          (-> system2 :duct.server/jetty :server)))
+          (is (= (:status @response) 200))
+          (is (= (:body @response) "bar"))
+          (finally
+            (ig/halt! system1)
+            (ig/halt! system2)))))
+
+    (testing "suspend and resume with different config"
+      (let [system1  (doto (ig/init config1) ig/suspend!)
+            config2' (assoc-in config2 [:duct.server/jetty :port] 3401)
+            system2  (ig/resume config2' system1)]
+        (try
+          (is (-> system1 :duct.server/jetty :server .isStopped))
+          (let [response (http/get "http://127.0.0.1:3401/")]
+            (is (= (:status response) 200))
+            (is (= (:body response) "bar")))
+          (finally
+            (ig/halt! system1)
+            (ig/halt! system2)))))
+
+    (testing "suspend and resume with extra config"
+      (let [system1 (doto (ig/init {}) ig/suspend!)
+            system2 (ig/resume config2 system1)]
+        (try
+          (let [response (http/get "http://127.0.0.1:3400/")]
+            (is (= (:status response) 200))
+            (is (= (:body response) "bar")))
+          (finally
+            (ig/halt! system2)))))))
