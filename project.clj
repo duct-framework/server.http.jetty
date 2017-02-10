ec77557ed2aee7a46fc98e96973a6bebc96b944b
James Reeves <jreeves@weavejester.com>
2017-02-10 22:47:51 +0000

    Add logging for starting and stopping server

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 [duct/core "0.9.0-SNAPSHOT"]
                  [integrant "0.2.1"]
                  [ring/ring-jetty-adapter "1.5.1"]]
   :profiles
--- src/duct/server/http/jetty.clj
+++ src/duct/server/http/jetty.clj
@@ -1,24 +1,30 @@
 (ns duct.server.http.jetty
   (:import org.eclipse.jetty.server.Server)
-  (:require [integrant.core :as ig]
+  (:require [duct.core.protocols :as p]
+            [integrant.core :as ig]
             [ring.adapter.jetty :as jetty]))
 
 (derive :duct.server.http/jetty :duct.server/http)
 
-(defmethod ig/init-key :duct.server.http/jetty [_ opts]
+(defmethod ig/init-key :duct.server.http/jetty [_ {:keys [logger] :as opts}]
   (let [handler (atom (delay (:handler opts)))
-        options (-> opts (dissoc :handler) (assoc :join? false))]
+        options (-> opts
+                    (dissoc :handler :logger)
+                    (assoc :join? false))]
+    (p/log logger :info ::starting-server (select-keys opts [:port]))
     {:handler handler
+     :logger  logger
      :server  (jetty/run-jetty (fn [req] (@@handler req)) options)}))
 
-(defmethod ig/halt-key! :duct.server.http/jetty [_ {:keys [server]}]
+(defmethod ig/halt-key! :duct.server.http/jetty [_ {:keys [server logger]}]
+  (p/log logger :info ::stopping-server)
   (.stop ^Server server))
 
 (defmethod ig/suspend-key! :duct.server.http/jetty [_ {:keys [handler]}]
   (reset! handler (promise)))
 
 (defmethod ig/resume-key :duct.server.http/jetty [key opts old-opts old-impl]
-  (if (= (dissoc opts :handler) (dissoc old-opts :handler))
+  (if (= (dissoc opts :handler :logger) (dissoc old-opts :handler :logger))
     (do (deliver @(:handler old-impl) (:handler opts))
         old-impl)
     (do (ig/halt-key! key old-impl)
