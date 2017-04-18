1090d6118daab878e04aefcba8933d1063328a8c
James Reeves <jreeves@weavejester.com>
2017-04-18 17:07:59 +0100

    Update Ring Jetty adapter to 1.6.0-RC3

--- project.clj
+++ project.clj
@@ -6,6 +6,6 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [duct/logger "0.1.1"]
                  [integrant "0.3.3"]
-                 [ring/ring-jetty-adapter "1.6.0-RC2"]]
+                 [ring/ring-jetty-adapter "1.6.0-RC3"]]
   :profiles
   {:dev {:dependencies [[clj-http "2.1.0"]]}})
