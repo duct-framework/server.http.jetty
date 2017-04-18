7278492fbc3cb52479f874432c1dbc9b484bb0d9
James Reeves <jreeves@weavejester.com>
2017-04-18 17:10:00 +0100

    Release 0.1.2

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ framework.
 
 To install, add the following to your project `:dependencies`:
 
-    [duct/server.http.jetty "0.1.1"]
+    [duct/server.http.jetty "0.1.2"]
 
 ## Usage
 
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject duct/server.http.jetty "0.1.1"
+(defproject duct/server.http.jetty "0.1.2"
   :description "Integrant methods for running a Jetty web server"
   :url "https://github.com/duct-framework/server.http.jetty"
   :license {:name "Eclipse Public License"
