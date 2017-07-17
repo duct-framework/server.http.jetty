a213ebc6d3c3dabdc6c9fde2feda0a7f329c9d29
James Reeves <jreeves@weavejester.com>
2017-07-18 00:54:01 +0100

    Update Integrant to 0.4.1

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [duct/logger "0.1.1"]
-                 [integrant "0.4.0"]
+                 [integrant "0.4.1"]
                  [ring/ring-jetty-adapter "1.6.1"]]
   :profiles
   {:dev {:dependencies [[clj-http "2.1.0"]]}})
