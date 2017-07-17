78ea406be1e4924343ac1bbc0cdd94b2df9963d0
James Reeves <jreeves@weavejester.com>
2017-07-18 00:54:09 +0100

    Update Ring to 1.6.2

--- project.clj
+++ project.clj
@@ -6,6 +6,6 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [duct/logger "0.1.1"]
                  [integrant "0.4.1"]
-                 [ring/ring-jetty-adapter "1.6.1"]]
+                 [ring/ring-jetty-adapter "1.6.2"]]
   :profiles
   {:dev {:dependencies [[clj-http "2.1.0"]]}})
