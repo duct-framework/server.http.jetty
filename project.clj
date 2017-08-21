a11fabe358c859c918fe6102701a065f327e0782
James Reeves <jreeves@weavejester.com>
2017-08-21 15:04:29 +0100

    Update Clojure version to 1.9.0-alpha17

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/duct-framework/server.http.jetty"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
                  [duct/logger "0.1.1"]
                  [integrant "0.4.1"]
                  [ring/ring-jetty-adapter "1.6.2"]]
