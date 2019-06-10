e8b742a7cf2c2521f0010d192b29d7baabf98bb0
Gaspard <gaspard@zimpler.com>
2019-06-10 19:13:05 +0200

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "https://github.com/duct-framework/server.http.jetty"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
-                 [duct/core "0.6.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [duct/core "0.7.0"]
                  [duct/logger "0.1.1"]
-                 [integrant "0.6.1"]
-                 [ring/ring-jetty-adapter "1.6.2"]]
+                 [integrant "0.7.0"]
+                 [ring/ring-jetty-adapter "1.7.1"]]
   :profiles
-  {:dev {:dependencies [[clj-http "2.1.0"]]}})
+  {:dev {:dependencies [[clj-http "3.10.0"]]}})
