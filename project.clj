6f0cb4ff96f03aac9aa703307951b6318f2f540a
James Reeves <jreeves@weavejester.com>
2020-08-07 16:34:59 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [duct/core "0.7.0"]
+                 [duct/core "0.8.0"]
                  [duct/logger "0.3.0"]
-                 [integrant "0.7.0"]
-                 [ring/ring-jetty-adapter "1.7.1"]]
+                 [integrant "0.8.0"]
+                 [ring/ring-jetty-adapter "1.8.1"]]
   :profiles
-  {:dev {:dependencies [[clj-http "3.10.0"]]}})
+  {:dev {:dependencies [[clj-http "3.10.2"]]}})
