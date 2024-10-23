5c424ae87a24bbac41ce265417fd4c6b1d12ba91
Erwin Rooijakkers <erwin@rooijakkers.software>
2024-10-23 20:25:05 +0200

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "https://github.com/duct-framework/server.http.jetty"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.3"]
-                 [duct/core "0.8.0"]
+  :dependencies [[org.clojure/clojure "1.11.4"]
+                 [duct/core "0.8.1"]
                  [duct/logger "0.3.0"]
-                 [integrant "0.8.0"]
-                 [ring/ring-jetty-adapter "1.9.2"]]
+                 [integrant "0.13.0"]
+                 [ring/ring-jetty-adapter "1.13.0"]]
   :profiles
   {:dev {:dependencies [[clj-http "3.12.1"]]}})
