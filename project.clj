2d42910099fd040024aeab61fadb5f8ddab709ea
James Reeves <jreeves@weavejester.com>
2026-06-11 23:55:44 +0100

    Update Clojure dependency

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/duct-framework/server.http.jetty"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.12.3"]
+  :dependencies [[org.clojure/clojure "1.12.5"]
                  [org.duct-framework/logger "0.4.0"]
                  [integrant "1.0.1"]
                  [ring/ring-jetty-adapter "1.15.4"]]
